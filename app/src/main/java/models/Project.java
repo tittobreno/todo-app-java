9c79c4e04cc9a2be0a549d7fdac687434141a236
unknown <brenomoraes97@yahoo.com>
2023-04-10 15:48:31 -0300

    created models

---
+++ app/src/main/java/models/Project.java
@@ -0,0 +1,68 @@
+package models;
+
+import java.time.LocalDateTime;
+
+public class Project {
+
+    private int id;
+    private String name;
+    private String description;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Project(int id, String name, String description, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    };
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" + "id=" + id + ", name=" + name + ", description=" + description + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + '}';
+    }
+    
+    
+
+}
---
+++ app/src/main/java/models/Task.java
@@ -0,0 +1,32 @@
+
+package models;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class Task {
+    private int id;
+    private int idProject;
+    private String name;
+    private String description;
+    private String notes;
+    private boolean isCompleted;
+    private Date deadline;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Task(int id, int idProject, String name, String description, String notes, boolean isCompleted, Date deadline, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.idProject = idProject;
+        this.name = name;
+        this.description = description;
+        this.notes = notes;
+        this.isCompleted = isCompleted;
+        this.deadline = deadline;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+    
+    
+    
+}
