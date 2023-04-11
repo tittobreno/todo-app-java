4cf3d45f0b3d0fb958be69f3b54e6a51f254be99
unknown <brenomoraes97@yahoo.com>
2023-04-11 01:36:42 -0300

    feat: created task controller and method for delete task

---
+++ app/src/main/java/controllers/TaskController.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import models.Task;
+import utils.ConnectionFactory;
+
+public class TaskController {
+    
+    public void save(Task task) {
+        
+    }
+    
+    public void update(Task task) {
+        
+    }
+    
+    public void removeById(int taskId) throws SQLException, ClassNotFoundException{
+        
+        String sql = "DELETE FROM tasks WHERE id = ?";
+        
+        Connection conn = null;
+        PreparedStatement statement = null;
+        
+        try {
+            conn = ConnectionFactory.getConnection();
+            statement = conn.prepareStatement(sql);
+            statement.setInt(1, taskId);
+            statement.execute();
+        } catch (SQLException e) {
+        throw new SQLException("Erro ao deletar a tarefa");
+        } finally {
+            ConnectionFactory.closeConnection(conn);
+        }
+        
+    }
+    
+    public List<Task> getAll(int idProject){
+        return null;
+    }
+    
+    
+}
