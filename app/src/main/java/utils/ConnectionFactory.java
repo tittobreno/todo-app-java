9608f5e9e58658d876a6991b584ba3eb5c737019
unknown <brenomoraes97@yahoo.com>
2023-04-11 01:05:52 -0300

    feat: create database connection

---
+++ app/src/main/java/utils/ConnectionFactory.java
@@ -0,0 +1,36 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class ConnectionFactory {
+    
+    private static final Dotenv dotenv = Dotenv.load();
+    
+    public static final String DRIVER = dotenv.get("DRIVER");
+    public static final String URL = dotenv.get("URL");
+    public static final String USER = dotenv.get("DB_USER");
+    public static final String PASSWORD = dotenv.get("DB_PASSWORD");
+
+    public static Connection getConnection() throws ClassNotFoundException {
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro na conexão com o banco de dados", e);
+        }
+    }
+
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao fechar conexão com o banco de dados", e);
+        }
+    }
+
+};
