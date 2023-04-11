80ba268ea7d2ef2db9855056d9fa8b9e32e8347b
unknown <brenomoraes97@yahoo.com>
2023-04-11 02:38:01 -0300

    feat: created method for save task

--- app/src/main/java/controllers/TaskController.java
+++ app/src/main/java/controllers/TaskController.java
@@ -1,45 +1,85 @@
 package controllers;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 import models.Task;
 import utils.ConnectionFactory;
 
 public class TaskController {
-    
-    public void save(Task task) {
-        
+
+    public void save(Task task) throws SQLException, ClassNotFoundException {
+
+        String sql = "INSERT INTO tasks (idProject,"
+                + "name,"
+                + "description,"
+                + "completed,"
+                + "notes,"
+                + "deadline,"
+                + "createdAt,"
+                + "updatedAt) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+        Connection connection = null;
+        PreparedStatement statement = null;
+
+        try {
+            connection = ConnectionFactory.getConnection();
+            statement = connection.prepareStatement(sql);
+
+            statement.setInt(1, task.getIdProject());
+            statement.setString(2, task.getName());
+            statement.setString(3, task.getDescription());
+            statement.setBoolean(4, task.isIsCompleted());
+            statement.setString(5, task.getNotes());
+            statement.setDate(6, new Date(task.getDeadline().getTime()));
+
+            LocalDateTime createdAt = task.getCreatedAt();
+            statement.setTimestamp(7, Timestamp.valueOf(createdAt));
+
+            LocalDateTime updatedAt = task.getUpdatedAt();
+            statement.setTimestamp(7, Timestamp.valueOf(updatedAt));
+            statement.execute();
+            
+            
+        } catch (ClassNotFoundException | SQLException ex) {
+            throw new RuntimeException("Erro ao salvar nova tarefa" + 
+                    ex.getMessage(), ex);
+        } finally {
+            ConnectionFactory.closeConnection(connection);
+        }
+
     }
-    
+
     public void update(Task task) {
-        
+
     }
-    
-    public void removeById(int taskId) throws SQLException, ClassNotFoundException{
-        
+
+    public void removeById(int taskId) throws SQLException, ClassNotFoundException {
+
         String sql = "DELETE FROM tasks WHERE id = ?";
-        
+
         Connection conn = null;
         PreparedStatement statement = null;
-        
+
         try {
             conn = ConnectionFactory.getConnection();
             statement = conn.prepareStatement(sql);
             statement.setInt(1, taskId);
             statement.execute();
         } catch (SQLException e) {
-        throw new SQLException("Erro ao deletar a tarefa");
+            throw new SQLException("Erro ao deletar a tarefa");
         } finally {
             ConnectionFactory.closeConnection(conn);
         }
-        
+
     }
-    
-    public List<Task> getAll(int idProject){
+
+    public List<Task> getAll(int idProject) {
         return null;
     }
-    
-    
+
 }
--- app/src/main/java/models/Task.java
+++ app/src/main/java/models/Task.java
@@ -2,6 +2,7 @@
 package models;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 
 public class Task {
@@ -26,6 +27,85 @@ public class Task {
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
     }
+    
+   public Task() {
+    this.createdAt = LocalDateTime.ofInstant(new Date().toInstant(), ZoneId.systemDefault());
+}
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdProject() {
+        return idProject;
+    }
+
+    public void setIdProject(int idProject) {
+        this.idProject = idProject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public boolean isIsCompleted() {
+        return isCompleted;
+    }
+
+    public void setIsCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    
+    
 
     @Override
     public String toString() {
